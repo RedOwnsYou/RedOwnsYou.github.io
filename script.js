77d7632f521623cfd3a54583dcd18aa0fa0896f4
RedOwnsYou <tugrateke33@gmail.com>
2025-12-07 18:07:15 +0300

    Fix display initialization and clear functions

--- script.js
+++ script.js
@@ -2,16 +2,39 @@ console.log("Calculator script loaded.");
 
 let display = document.getElementById("display");
 
+display.value = "0";
+
 function appendToDisplay(value) {
+  if (display.value === "0" && /[0-9.]/.test(value)) {
+    display.value = value;
+    return;
+  }
+  if (display.value === "Undefined" || display.value === "ERROR") {
+    if (/[0-9.]/.test(value)) {
+      display.value = value;
+    } else {
+      display.value = "0" + value;
+    }
+    return;
+  }
   display.value += value;
 }
 
 function clearDisplay() {
-  display.value = " ";
+  display.value = "0";
 }
 
 function clearLast() {
-  display.value = display.value.slice(0, -1);
+  if (display.value === "undefined" || display.value === "ERROR") {
+    clearDisplay();
+    return;
+  }
+
+  if (display.value.length > 1) {
+    display.value = display.value.slice(0, -1);
+  } else {
+    display.value = "0";
+  }
 }
 
 function calculate() {
